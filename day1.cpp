a14956f15056e91ff1416837438cf59725f61b71
Caleb Meyer <151958959+cablecorn@users.noreply.github.com>
2024-12-01 20:54:01 -0600

    Create day1.cpp

---
+++ day1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+int main(){
+  std::string file;
+  std::cout << "Enter filename to read: ";
+  std::cin >> file;
+  std::ifstream infile;
+  infile.open(file);
+  if (!infile){
+    std::cout << "This list does not exist!\n";
+    return 1;
+  }
+
+  std::vector<int> list1;
+  std::vector<int> list2;
+  int temp {};
+  infile >> temp;
+  while(infile){
+    list1.push_back(temp);
+    infile >> temp;
+    list2.push_back(temp)
+    infile >> temp;
+  }
+  infile.close();
+
+  std::sort(list1.begin(), list1.end());
+  std::sort(list2.begin(), list2.end());
+
+  int tot {};
+  for (int i{0}; i < list1.size(); i++)
+  
+  return 0;
+}
